8d8f07e4e86129b490061c2a934e46085872d49a
seveibar <seveibar@gmail.com>
2019-07-05 19:45:35 -0400

    create pipeline instance initial implementation

---
+++ packages/builtin-stages/CreatePipelineInstance/CreatePipelineInstance.js
@@ -0,0 +1,30 @@
+// @flow
+
+const { send, json } = require("micro")
+const got = require("got")
+
+module.exports = async (req, res) => {
+  let { instance_id, inputs, state } = await json(req)
+
+  if (!state) state = {}
+  if (!state.startTime) state.startTime = Date.now()
+
+  if (inputs.delay.value) {
+    if (Date.now() - state.startTime < inputs.delay.value) {
+      return {
+        state,
+        progress: (Date.now() - state.startTime) / inputs.delay.value
+      }
+    }
+  }
+
+  const { value } = inputs.input
+
+  return {
+    complete: true,
+    state,
+    outputs: {
+      output: { value }
+    }
+  }
+}
