b272736a241845a83af6e493f0b77a9dd0920aad
seveibar <seveibar@gmail.com>
2020-01-04 17:54:28 -0500

    database can now start rest api

--- packages/database/index.js
+++ packages/database/index.js
@@ -1,41 +1,45 @@
 // @flow
 
-const path = require("path")
-const qspg = require("qspg").default
-const runSeed = require("./seed")
-const { spawn } = require("child_process")
-const getPort = require("get-port")
+const path = require("path");
+const qspg = require("qspg").default;
+const runSeed = require("./seed");
+const { spawn } = require("child_process");
+const getPort = require("get-port");
 
 module.exports = async ({
   testMode = false,
   seed = false,
   restAPI = false
 } = {}) => {
-  if (!process.env.POSTGRES_DB) process.env.POSTGRES_DB = "etl9"
+  if (!process.env.POSTGRES_DB) process.env.POSTGRES_DB = "etl9";
   const conn = await qspg({
     migrationsDir: path.resolve(__dirname, "./migrations"),
 
     // Optional: If set to true, QSPG will create a database with a randomized name, this is
     // great for unit tests. Defaults to true only if USE_TEST_DB env var is set.
     testMode
-  })
+  });
 
   if (seed) {
-    await runSeed(conn)
+    await runSeed(conn);
   }
 
   if (restAPI) {
-    console.log("starting db REST api (postgrest)")
-    const restAPIProjectDir = path.resolve(__dirname, "..", "database-rest-api")
-    conn.restAPIPort = await getPort()
-    conn.restAPIURL = `http://localhost:${conn.restAPIPort}`
+    console.log("starting db REST api (postgrest)");
+    const restAPIProjectDir = path.resolve(
+      __dirname,
+      "..",
+      "database-rest-api"
+    );
+    conn.restAPIPort = await getPort();
+    conn.restAPIURL = `http://localhost:${conn.restAPIPort}`;
     const {
       host: POSTGRES_HOST,
       port: POSTGRES_PORT,
       user: POSTGRES_USER,
       password: POSTGRES_PASS,
       database: POSTGRES_DB
-    } = conn.client.config.connection
+    } = conn.client.config.connection;
     const restAPIProcess = spawn(
       "postgrest",
       [path.resolve(restAPIProjectDir, "postgrest.conf")],
@@ -51,35 +55,32 @@ module.exports = async ({
           PORT: conn.restAPIPort
         }
       }
-    )
-    let connectionSuccessfulCallback
+    );
+    let connectionSuccessfulCallback;
     restAPIProcess.stdout.on("data", data => {
-      console.log("DB REST:", data.toString())
+      console.log("DB REST:", data.toString());
       if (
         data
           .toString()
           .toLowerCase()
           .includes("connection successful")
       ) {
-        connectionSuccessfulCallback()
+        connectionSuccessfulCallback();
       }
-    })
+    });
     restAPIProcess.stderr.on("data", data => {
-      console.log("DB REST ERR:", data.toString())
-      console.error("DB REST ERR:", data.toString())
-    })
-    const oldDestroy = conn.destroy
-    conn.destroy = () => {
-      console.log("destroying")
-      restAPIProcess.kill()
-      return oldDestroy()
-    }
-    await new Promise(resolve => (connectionSuccessfulCallback = resolve))
-    console.log("Postgrest started")
+      console.log("DB REST ERR:", data.toString());
+      console.error("DB REST ERR:", data.toString());
+    });
+    conn.destroyHooks.push(() => {
+      restAPIProcess.kill();
+    });
+    await new Promise(resolve => (connectionSuccessfulCallback = resolve));
+    console.log("Postgrest started");
   } else {
-    conn.restAPIPort = 9102
-    conn.restAPIURL = `http://localhost:9102`
+    conn.restAPIPort = 9102;
+    conn.restAPIURL = `http://localhost:9102`;
   }
 
-  return conn
-}
+  return conn;
+};
--- packages/database/tests/test.js
+++ packages/database/tests/test.js
@@ -1,39 +1,42 @@
 // @flow
 // require("leaked-handles")
 
-var test = require("ava")
-var getDB = require("../")
-var request = require("request-promise")
-//
-// test("connect and disconnect", async t => {
-//   t.plan(2)
-//   const db = await getDB({ testMode: true })
-//
-//   const res = await db.raw("SELECT 1+1 as sum")
-//   t.assert(res.rows[0].sum === 2)
-//
-//   await db.destroy()
-//   t.pass("should destroy connection")
-// })
-//
-// test("seed", async t => {
-//   t.plan(2)
-//   const db = await getDB({ seed: true, testMode: true })
-//
-//   t.assert((await db("definition")).length >= 10)
-//
-//   await db.destroy()
-//   t.pass("should destroy connection")
-// })
+var test = require("ava");
+var getDB = require("../");
+var request = require("request-promise");
+
+test("connect and disconnect", async t => {
+  t.plan(2);
+  const db = await getDB({ testMode: true });
+
+  const res = await db.raw("SELECT 1+1 as sum");
+  t.assert(res.rows[0].sum === 2);
+
+  await db.destroy();
+  t.pass("should destroy connection");
+});
+
+test("seed", async t => {
+  t.plan(2);
+  const db = await getDB({ seed: true, testMode: true });
+
+  t.assert((await db("definition")).length >= 10);
+
+  await db.destroy();
+  t.pass("should destroy connection");
+});
 
 test("start rest api", async t => {
-  const db = await getDB({ testMode: true, seed: true, restAPI: true })
+  const db = await getDB({ testMode: true, seed: true, restAPI: true });
 
-  t.truthy(db.restAPIURL)
-  t.assert(!db.restAPIURL.includes("9102"))
-  const defs = await request({ uri: db.restAPIURL + "/definition", json: true })
-  t.assert(defs.length >= 10)
+  t.truthy(db.restAPIURL);
+  t.assert(!db.restAPIURL.includes("9102"));
+  const defs = await request({
+    uri: db.restAPIURL + "/definition",
+    json: true
+  });
+  t.assert(defs.length >= 10);
 
-  await db.destroy()
-  t.pass("should destroy connection")
-})
+  await db.destroy();
+  t.pass("should destroy connection");
+});
