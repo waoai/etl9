b3abb95fb71aac420d8cdf2a501fd55b5318d4c9
seveibar <seveibar@gmail.com>
2020-01-06 17:20:06 -0500

    fixed instance_id reference

--- packages/database/migrations/v02.sql
+++ packages/database/migrations/v02.sql
@@ -1,6 +1,6 @@
 CREATE TABLE system_instance_profile (
   id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
-  instance_id text references instance NOT NULL,
+  instance_id text NOT NULL,
   started_at timestamptz NOT NULL DEFAULT current_timestamp,
   ended_at timestamptz,
   duration float GENERATED ALWAYS AS (EXTRACT(EPOCH FROM (ended_at - started_at))) STORED
@@ -8,7 +8,7 @@ CREATE TABLE system_instance_profile (
 
 CREATE TABLE system_stage_profile (
   id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
-  instance_id text references instance NOT NULL,
+  instance_id text NOT NULL,
   stage_id text NOT NULL,
   stage_name text NOT NULL,
   started_at timestamptz NOT NULL DEFAULT current_timestamp,
